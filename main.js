4c55439225dfbda2b2d372201431f69df5e6b88e
fifimol <44194211+fifimol@users.noreply.github.com>
2021-08-04 01:25:12 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+const {app, BrowserWindow, Menu, MenuItem} = require('electron')
+const url = require('url')
+const path = require('path')
+
+let win
+
+function createWindow() {
+   win = new BrowserWindow({width: 1280, height: 768,minWidth: 1024,
+        minHeight: 768})
+   win.loadURL(url.format ({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+   }))
+}
+
+const template = [
+   {
+      label: 'Menu',
+      submenu: [
+         {
+            role: 'undo'
+         },
+         {
+            role: 'redo'
+         },
+         {
+            type: 'separator'
+         },
+{
+            role: 'selectAll'
+         },
+         {
+            role: 'copy'
+         }
+       
+      ]
+   },
+   
+   {
+      role: 'window',
+      submenu: [
+         {
+            role: 'minimize'
+         },
+         {
+            role: 'close'
+         }
+      ]
+   },
+   {
+      label: 'Version',
+      submenu: [
+         {
+            role: 'undo'
+         },
+   
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+app.on('ready', createWindow)
